691b3940687b74bf678c6ae841d2cc9ae097501c
zeffii <tetha.z@gmail.com>
2017-10-03 10:52:37 +0200

    move to bmesh

--- nodes/modifier_make/cross_section.py
+++ nodes/modifier_make/cross_section.py
@@ -17,11 +17,12 @@
 # ##### END GPL LICENSE BLOCK #####
 
 import bpy
+import bmesh
 from bpy.props import BoolProperty
 from mathutils import Vector, Matrix
-
 from sverchok.node_tree import SverchCustomTreeNode
 from sverchok.data_structure import updateNode, Vector_generate, Vector_degenerate
+from sverchok.utils.sv_bmesh_utils import bmesh_from_pydata
 
 
 def section(cut_me_vertices, cut_me_edges, mx, pp, pno, FILL=False, TRI=True):
@@ -34,7 +35,6 @@ def section(cut_me_vertices, cut_me_edges, mx, pp, pno, FILL=False, TRI=True):
              Boolean - False if no section exists"""
 
     def equation_plane(point, normal_dest):
-        # получаем коэффициенты уравнения плоскости по точке и нормали
         normal = normal_dest.normalized()
         A = normal.x
         B = normal.y
@@ -51,10 +51,7 @@ def section(cut_me_vertices, cut_me_edges, mx, pp, pno, FILL=False, TRI=True):
 
     def point_on_plane(v1, ep):
         formula = ep[0]*v1.x+ep[1]*v1.y+ep[2]*v1.z+ep[3]
-        if formula == 0.0:
-            return True
-        else:
-            return False
+        return formula == 0.0
 
     if not cut_me_edges or not cut_me_vertices:
         return False
@@ -68,20 +65,16 @@ def section(cut_me_vertices, cut_me_edges, mx, pp, pno, FILL=False, TRI=True):
     if len(cut_me_edges[0]) > 2:
         cut_me_polygons = cut_me_edges.copy()
         cut_me_edges = []
-
-    new_me = bpy.data.meshes.new('tempus')
-    new_me.from_pydata(cut_me_vertices, cut_me_edges, cut_me_polygons)
-    new_me.update(calc_edges=True)
-
-    for ed_idx, ed in enumerate(new_me.edges):
+    
+    new_bm = bmesh_from_pydata(cut_me_vertices, cut_me_edges, cut_me_polygons, True)
+    for ed_idx, ed in enumerate(new_bm.edges):
         # getting a vector from each edge vertices to a point on the plane
         # first apply transformation matrix so we get the real section
 
-        vert1 = ed.vertices[0]
-        v1 = new_me.vertices[vert1].co * mx.transposed()
-
-        vert2 = ed.vertices[1]
-        v2 = new_me.vertices[vert2].co * mx.transposed()
+        vert1 = ed.verts[0]
+        v1 = vert1.co * mx.transposed()
+        vert2 = ed.verts[1]
+        v2 = vert2.co * mx.transposed()
 
         vec = v2-v1
         mul = vec * pno
@@ -99,86 +92,57 @@ def section(cut_me_vertices, cut_me_edges, mx, pp, pno, FILL=False, TRI=True):
         pq = vec*t0+v1
         if (pq-v1).length <= vec.length and (pq-v2).length <= vec.length:
             verts.append(pq)
-            ed_xsect[ed.key] = len(ed_xsect)
+            ed_xsect[ed.index] = len(ed_xsect)
 
     edges = []
-    for f in new_me.polygons:
+    for f in new_bm.faces:
         # get the edges that the intersecting points form
         # to explain this better:
         # If a face has an edge that is proven to be crossed then use the
         # mapping we created earlier to connect the edges properly
-        ps = [ed_xsect[key] for key in f.edge_keys if key in ed_xsect]
+        ps = [ed_xsect[key.index] for key in f.edges if key in ed_xsect]
 
         if len(ps) == 2:
             edges.append(tuple(ps))
 
     x_me['Verts'] = verts
     x_me['Edges'] = edges
-    bpy.data.meshes.remove(new_me)
+    del new_bm
     if x_me:
         if edges and FILL:
-            me = bpy.data.meshes.new('Section')
-            me.from_pydata(verts, edges, [])
-
-            # create a temp object and link it to the current scene to be able to
-            # apply rem Doubles and fill
-            tmp_ob = bpy.data.objects.new('Mesh', me)
-
-            sce = bpy.context.scene
-            sce.objects.link(tmp_ob)
-
-            # do a remove doubles to cleanup the mesh, this is needed when there
-            # is one or more edges coplanar to the plane.
-            sce.objects.active = tmp_ob
-
-            bpy.ops.object.mode_set(mode="EDIT")
-            bpy.ops.mesh.select_mode(type="EDGE", action="ENABLE")
-            bpy.ops.mesh.select_all(action="SELECT")
-
-            # remove doubles:
-            bpy.ops.mesh.remove_doubles()
+            bm = bmesh_from_pydata(verts, edges, [])
+            bmesh.ops.remove_doubles(bm, verts=bm.verts[:], dist=0.00001) 
 
             # one or not one polygon? here is the answer!
-            if TRI:
-                bpy.ops.mesh.edge_face_add()
-            else:
-                bpy.ops.mesh.fill()
-                bpy.ops.mesh.tris_convert_to_quads()
+            fres = bmesh.ops.edgenet_prepare(bm, edges=bm.edges[:])
+            # bmesh.ops.edgeloop_fill(bm, edges=fres['edges'])
+            bmesh.ops.contextual_create(bm, fres['edges'])
+            bmesh.ops.recalc_face_normals(bm)
 
-            # recalculate outside normals:
-            bpy.ops.mesh.normals_make_consistent(inside=False)
-
-            bpy.ops.object.mode_set(mode='OBJECT')
             pols = []
-            for p in me.polygons:
+            for p in bm.faces:
                 vs = []
-                for v in p.vertices:
+                for v in p.verts:
                     vs.append(v)
                 pols.append(vs)
 
             verts = []
-            for v in me.vertices:
+            for v in bm.verts:
                 verts.append(v.co)
 
             if not pols:
-                bpy.ops.object.mode_set(mode='EDIT')
-                bpy.ops.mesh.select_mode(type="VERT", action="ENABLE")
-                bpy.ops.mesh.select_all(action="SELECT")
-                bpy.ops.mesh.edge_face_add()
-                bpy.ops.object.mode_set(mode='OBJECT')
-
-                for p in me.polygons:
+                geom = bm.verts[:] + bm.edges[:]
+                bmesh.ops.contextual_create(bm, geom)
+                for p in bm.faces:
                     vs = []
-                    for v in p.vertices:
+                    for v in p.verts:
                         vs.append(v)
                     pols.append(vs)
 
             x_me['Verts'] = verts
             x_me['Edges'] = pols
 
-            # Cleanup
-            sce.objects.unlink(tmp_ob)
-            del tmp_ob
+            del bm
         return x_me
     else:
         return False
